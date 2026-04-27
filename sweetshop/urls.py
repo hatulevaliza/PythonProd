bcb89ff2f537633b36e81a5687165b35aa4f7bff
Liza <hatulevaliza@gmail.com>
2026-04-28 00:02:34 +0300

    Добавлено отображение картинок товаров, настроены медиа-файлы

--- sweetshop/urls.py
+++ sweetshop/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
 from shop import views
 
 urlpatterns = [
@@ -31,3 +33,6 @@ urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('profile/', views.profile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
